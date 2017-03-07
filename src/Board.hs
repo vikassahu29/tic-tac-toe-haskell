867e74e277fed508756d63ac99e4707f2d5c2b6a
Vikas Sahu <vikassahu2904@gmail.com>
2017-03-07 18:09:15 +0530

    minor fixes

--- src/Board.hs
+++ src/Board.hs
@@ -18,39 +18,33 @@ module Board
     getCellAtPos b (x,y) = (b!!x)!!y
 
     isEmptyCell :: Cell -> Bool
-    isEmptyCell c = if c==Empty then True else False
+    isEmptyCell Empty = True
+    isEmptyCell _ = True
 
     checkBounds :: (Int, Int) -> Bool
-    checkBounds (x,y) = if (x>2 || y>2) then False else True     -- negetive cases?
+    checkBounds (x,y) = (x < 3 &&  x > -1 ) && (y < 3 && y > -1)    -- negetive cases?
 
     checkValidMove :: Board -> (Int, Int) -> Bool
-    checkValidMove b (x,y) = 
-        if (checkBounds (x,y)) && (isEmptyCell $ getCellAtPos b (x,y)) 
-            then 
-                True 
-            else 
-                False
+    checkValidMove b (x,y) = (checkBounds (x,y)) && (isEmptyCell $ getCellAtPos b (x,y))
 
-    makeTurn :: Board -> (Int, Int) ->TurnType -> Board
-    makeTurn b (x,y) t = 
-        if (checkValidMove b (x,y))
-            then if x==0
-                    then [makeTurnHelper (b!!0) y t] ++ [b!!1] ++ [b!!2]
-                    else if x == 1
-                        then [b!!0] ++ [makeTurnHelper (b!!1) y t] ++ [b!!2]
-                        else [b!!0] ++ [b!!1] ++ [makeTurnHelper (b!!2) y t]
-            else b
+    makeTurn :: Board -> (Int, Int) -> TurnType -> Board
+    makeTurn b (x,y) t =
+      | checkValidMove b (x, y) =
+          if x==0
+          then [makeTurnHelper (b!!0) y t] ++ [b!!1] ++ [b!!2]
+          else if x == 1
+            then [b!!0] ++ [makeTurnHelper (b!!1) y t] ++ [b!!2]
+            else [b!!0] ++ [b!!1] ++ [makeTurnHelper (b!!2) y t]
+      | otherwise = b
 
     makeTurnHelper :: [Cell] -> Int -> TurnType -> [Cell]
-    makeTurnHelper (x:xs) y t = 
-        if (y==0) 
-            then [TurnType t] ++ xs
-            else [x] ++ (makeTurnHelper xs (y-1) t)
+    makeTurnHelper (x:xs) 0 t = [TurnType t] ++ xs
+    makeTurnHelper (x:xs) y t = [x] ++ (makeTurnHelper xs (y-1) t)
 
     -- hasPlayerAtThisPosWon: We only check for flows starting with current pos/cell
     -- A more general version of it would check all the possible ways
     hasPlayerAtThisPosWon :: Board -> (Int, Int) -> Bool
-    hasPlayerAtThisPosWon b (x,y) = 
+    hasPlayerAtThisPosWon b (x,y) =
         hasPlayerAtThisPosWonHelper b (x,y) False False ||
         hasPlayerAtThisPosWonHelper b (x,y) False True  ||
         hasPlayerAtThisPosWonHelper b (x,y) True False  ||
@@ -59,14 +53,14 @@ module Board
     -- Look at this beautiful declaration :P
     hasPlayerAtThisPosWonHelper :: Board -> (Int, Int) -> Bool -> Bool -> Bool
     hasPlayerAtThisPosWonHelper b (x,y) isDiag isVert =
-        let 
-        xdist = 
-                if (isDiag) 
+        let
+        xdist =
+                if (isDiag)
                     then 1
                     else if (not isVert)
                             then 0
                             else 1
-        ydist = 
+        ydist =
                 if (isDiag)
                     then if (isVert) then (-1) else 1
                     else if (isVert) then 1 else 0
@@ -75,8 +69,8 @@ module Board
         p3 = getCellAtPos b ((x+2*xdist)`mod`3, (y+2*ydist)`mod`3)
 
         in
-            isSame p1 p2 p3 
-            
+            isSame p1 p2 p3
+
 
     isSame :: Cell -> Cell -> Cell -> Bool
     isSame (TurnType Cross) (TurnType Cross) (TurnType Cross) = True
